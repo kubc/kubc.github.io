fe1fb70872f9832f16f673ad8d99233cc674361b
Kohei Namba <kubc.stu@gmail.com>
2018-03-24 14:17:31 +0900

    initial commit.

---
+++ docs/common.js
@@ -0,0 +1,42 @@
+let mapLoaded = false;
+if (window.history && window.history.pushState) {
+	const _hostname = location.hostname;
+	window.history.replaceState(_hostname, null, location.pathname);
+	const updateMenu = () => {
+		const current = $('#currentPage').text();
+		$('nav a').each((i, e) => {
+			const target = $(e);
+			if (current === target.attr('title')) {
+				target.addClass('current');
+			} else {
+				target.removeClass('current');
+			}
+		});
+		if (current === 'index') {
+			$.getScript('index.js', () => {
+				if (mapLoaded) {
+					initMap();
+				} else {
+					$.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCG6yw4tPrjC4MmYVXXNXYqh5KbYGscBrU&callback=initMap');
+					mapLoaded = true;
+				}
+			});
+		}
+	};
+	const loadContents = url => {
+		$('main').load(url + ' main > *', () => {
+			updateMenu();
+		});
+	};
+	updateMenu();
+	$(document).on('click', 'nav a, .ps', e => {
+		e.preventDefault();
+		const url = $(e.currentTarget).attr('href');
+		loadContents(url);
+		window.history.pushState(_hostname, null, url);
+	});
+	$(window).on('popstate', e => {
+		if (e.originalEvent.state != _hostname) return;
+		loadContents(location.pathname);
+	});
+}
