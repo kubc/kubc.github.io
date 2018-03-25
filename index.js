a05903a903db80128d3a3cfb03dfa0fcc938573c
Kohei Namba <kubc.stu@gmail.com>
2018-03-25 20:14:03 +0900

    make map zoom different between mobiles and the others.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function initMap() {
       lat: 35.025,
       lng: 135.78
     },
-    zoom: 16.5
+    zoom: /Mobi/.test(navigator.userAgent) ? 16: 17
   });
   const opts = [{
     position: new google.maps.LatLng(35.024, 135.77876),
